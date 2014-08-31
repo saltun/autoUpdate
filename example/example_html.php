9de47f4e6ade260b0b4130bc3c14eeda24b4df3f
saltun <savascanaltun@gmail.com>
2014-08-31 17:44:00 +0300

    example html

---
+++ example/example_html.php
@@ -0,0 +1,12 @@
+<?php
+require_once "autoUpdate.class.php";
+$autoUpdate=new autoUpdate;
+$autoUpdate->sourceurl="https://gist.githubusercontent.com/saltun/d1febfd82e12b57b6821/raw/d71b667279b1397b59ad1cb8f5c2419360434fc6/test.html";
+$autoUpdate->file="test.html";
+$autoUpdate->control();
+
+if ($autoUpdate) {
+	echo "Update";
+}
+
+?>
