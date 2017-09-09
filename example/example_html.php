95f137a9f32f9d0171f4dd3ae8a27a2ff99bf751
Savaş Can ALTUN <savascanaltun@gmail.com>
2017-09-10 01:10:17 +0300

    Update example_html.php

--- example/example_html.php
+++ example/example_html.php
@@ -1,5 +1,5 @@
 <?php
-require_once "autoUpdate.class.php";
+require_once "autoUpdate.php";
 $autoUpdate=new autoUpdate;
 $autoUpdate->sourceurl="https://gist.githubusercontent.com/saltun/d1febfd82e12b57b6821/raw/d71b667279b1397b59ad1cb8f5c2419360434fc6/test.html";
 $autoUpdate->file="test.html";
