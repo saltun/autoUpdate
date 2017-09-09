e204ecfb588bc493fbbaab1d48fcefc42d4955e5
Savaş Can ALTUN <savascanaltun@gmail.com>
2017-09-10 01:09:52 +0300

    Update index.php

--- example/index.php
+++ example/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once "autoUpdate.class.php";
+require_once "../autoUpdate.php";
 $autoUpdate=new autoUpdate;
 $autoUpdate->sourceurl="https://gist.githubusercontent.com/saltun/f0232a87c4cdc272ac70/raw/63c290ed54d0b98ab6ae7832052f0ca2aeb7fd38/testClass.php";
 $autoUpdate->file="testClass.php";
